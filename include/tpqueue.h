be9a2123467dd0b1d36836a4d04d46ce80eed61d
klovee <79109849+klovee@users.noreply.github.com>
2021-05-31 20:50:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,7 +6,57 @@
 template<typename T>
 class TPQueue {
   // Сюда помещается описание структуры "Очередь с приоритетами"
+   private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
+ public:
+explicit TPQueue(int = 100);
+~TPQueue();
+void push(const T &);
+    T pop();
+    T get() const;
+    bool isFull() const;
+    bool isEmpty() const;
 };
+
+// функция добавления элемента в очередь
+template<typename T>
+void TQueue<T>::push(const T & item)
+{
+    assert( count < size );
+ 
+    arr[end++] = item;
+    count++;
+
+
+    if (end > size)
+        end -= size + 1; // возвращаем end на начало очереди
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+        assert(count > 0);
+        T item = arr[begin++];
+        count--;
+        if (begin > size)
+                begin -= size + 1;
+        return item;
+}
+template<typename T>
+T TPQueue<T>::get() const {
+        assert(count > 0);
+        return arr[begin];
+}
+template<typename T>
+bool TPQueue<T>::isEmpty() const {
+        return count == 0;
+}
+template<typename T>
+bool TPQueue<T>::isFull() const {
+        return count == size;
+}
 
 struct SYM {
   char ch;
